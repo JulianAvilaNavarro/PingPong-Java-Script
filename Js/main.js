e987a4f58bb9c3cdcabc0b6d4ae018bca9caa719
Julian <julian.avila1994@gamil.com>
2021-11-18 15:34:44 -0500

    Se agregaron las barras usando helper methods.

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,6 @@
 (function(){
-    self.Board=function(withd,heigth){
-        this.withd=withd;
+    self.Board=function(width,heigth){
+        this.width=width;
         this.heigth=heigth;
         this.playing=false;
         this.game_over= false;
@@ -11,26 +11,67 @@
     self.Board.prototype={
         get elements(){
             var elements=this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
 })();
 
 (function(){
-    self.BoardView=function(canvas,board){
-        this.canvas=canvas;
-        this.canvas.withd=board.withd;
-        this.canvas.heigth=board.heigth;
+    self.Bar=function(x,y,width,heigth,board){
+        this.x=x;
+        this.y=y;
+        this.width=width;
         this.board=board;
-        this.ctx=canvas.getContext("2d");
-
+        this.heigth=heigth;
+        this.board.bars.push(this); //accedo al bar, al board y 
+        //le agrego nuevo elemento con push.
+        //dibujar cosas
+        this.kind="rectangle";
+    }
+    self.Bar.prototype={
+        down: function(){},
+        up: function(){}
     }
 })();
 
-window.addEventListener("load",main);
+
+
+(function(){
+    self.BoardView=function(canvas,board){
+        this.canvas=canvas;
+        this.canvas.width=board.width;
+        this.canvas.heigth=board.heigth;
+        this.board=board;
+        this.ctx=canvas.getContext("2d");
+    }
+    //helper method
+    self.BoardView.prototype={
+        draw: function(){
+            for (var i=this.board.elements.length-1;i>=0;i--){
+               var el= this.board.elements[i];
+               draw(this.ctx,el);
+            };
+        }
+    }
+    function draw(ctx,element){
+        if(element!==null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x,element.y,element.width,element.heigth);
+                    break;
+            }
+        }
+    }
+})();
+
+self.addEventListener("load",main);
 function main(){
-    var board= new Board(800,400);
-    var canvas=document.getElementById(canvas,board);
-    var boardView= new BoardView;
+    console.log("Hola Mundo");
+    var board= new Board(800,600);
+    var bar=new Bar(20,10,30,70,board);
+    var bar=new Bar(700,10,30,70,board);
+    var canvas=document.getElementById('canvas');
+    var board_View= new BoardView(canvas,board);
+    board_View.draw();
 }
